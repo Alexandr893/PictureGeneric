78bff6fe5d85aaf8657128917b079d2fe8ee345b
alexandr <Alexandr6732@mail.ru>
2022-11-10 14:13:25 +0700

    Unsplash api

---
+++ main.js
@@ -0,0 +1,77 @@
+// получаем инпут
+const input = document.getElementById('input');
+// получаем блок картинок(грид)
+const grid = document.getElementsByClassName('grid')[0];
+
+window.addEventListener('load',Theme);
+
+input.addEventListener('keydown',function(event){
+    // если пользователь нажал enter
+    if(event.key ==='Enter'){
+        loadImg();
+    }
+}
+);
+
+// так как каждый раз пользователь делает новый запрос предыдущие картинки должны удалятся 
+
+
+function loadImg(){
+    removeImg();
+
+    const url = 'https://api.unsplash.com/search/photos/?query='+input.value+'&per_page=9&client_id=hpi3Bl8XtNXzSb5bgIW8QZbv8wUt8cjRTISWJMCRYlE';
+    fetch(url)
+    // получили ответ объектом от запроса и преобразуем его
+    // то есть при получений ответа если его атрибут ok=true то вернуть его json-ом иначе вывести алерт со статусом ошибки
+    .then(response=> {
+        if(response.ok){
+           
+            return response.json();
+            
+        }
+        else{
+            alert(response.status);
+        }
+    })
+    // 2ой промис будет получать данные и выводить их,все картинки выводятся  массивом 
+    .then(data=>{
+        const NodeImgs = [];
+        // вывод по циклу
+        for(let i = 0; i<data.results.length;i++){
+            NodeImgs[i]=document.createElement('div');
+            NodeImgs[i].className='img';
+            NodeImgs[i].style.backgroundImage='url('+data.results[i].urls.raw+"&w=1366&h=768" +')';
+// возможность скачивать и открывать картинки в новом окне после двойного клика
+            NodeImgs[i].addEventListener('dblclick',function(){
+                window.open(data.results[i].links.download,'_blank');
+            })
+            grid.appendChild( NodeImgs[i]);
+        }
+    })
+}
+
+// функция удаления картинки
+function removeImg(){
+    grid.innerHTML='';
+}
+
+
+
+// реализация темной и светлой темы в зависимости от времени суток
+
+function Theme(){
+    const date = new Date();
+    const hour = date.getHours();
+
+
+    if(  hour > 19){
+        document.body.style.backgroundColor = "#393F47";
+        document.body.style.color = 'white';
+        
+    }
+    else{
+        document.body.style.backgroundColor = "whitesmoke";
+        document.body.style.color = 'black';
+    }
+}
+
